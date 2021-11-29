3cd5454c777d342c643293e98b4488854418f571
Stewart Platt <shteou@gmail.com>
2021-11-29 10:31:48 +0000

    feat: add Mode func

--- pkg/math/math.go
+++ pkg/math/math.go
@@ -18,3 +18,26 @@ func Mean[T constraints.Numeric](xs []T) T {
 func Average[T constraints.Numeric](xs []T) T {
 	return Mean(xs)
 }
+
+func Mode[T comparable](xs []T) T {
+	counts := map[T]int{}
+
+	for i, x := range xs {
+		if val, ok := counts[x]; ok {
+			counts[x] = counts[x] + 1
+		} else {
+			counts[x] = 1
+		}
+	}
+
+	var mostCommonCount := 0
+	var mostCommonKey T
+	for k, v := range counts {
+		if v > mostCommonCount {
+			mostCommonCount = v
+			mostCommonKey = k
+		}
+	}
+
+	return mostCommonKey
+}
