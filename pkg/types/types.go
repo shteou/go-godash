3b4109f213d4b54d250c63fd3f86903b1b3027a2
Stewart Platt <shteou@gmail.com>
2021-11-29 18:57:40 +0000

    docs: add go docs for all funcs/types

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -1,5 +1,15 @@
 package types
 
+// Mapper is a function which accepts one type
+// and returns another.
 type Mapper[T any, U any] func(T) U
+// Predicate is a function which accepts one type
+// and returns a bool indicating whether the predicate
+// was satisfied.
 type Predicate[T any] func(T) bool
+// Reducer is a function which accepts a parameter of
+// type T and a parameter of type U, the accumulator
+// and returns a new value of type U (this return value
+// is passed with the next element in the Reduce
+// function). 
 type Reducer[T any, U any] func(T, U) U
